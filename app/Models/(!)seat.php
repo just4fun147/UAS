5497d5f7ba185acab7ed23307a709ae59d8761c9
just4fun147 <pauluswindito1@gmail.com>
2022-12-05 11:06:39 +0700

    terminalDB

---
+++ app/Models/(!)seat.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+class seat extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'row',
+        'number',
+        'bus_id',
+        'jadwal',
+        'jam'
+    ];
+
+    public function getCreatedAttribute(){
+        if(!is_null($this->attributes['created_at'])){
+            return Carbon::parse($this->attributes['created_at'])->format('Y-m-d H:i:s');
+        }
+    }
+    
+    public function getUpdateAtAttribute(){
+        if(!is_null($this->attributes['update_at'])){
+            return Carbon::parse($this->attributes['update_at'])->format('Y-m-d H:i:s');
+        }
+    }
+}
